be8da318df2dbaf867af30196f605c53cce2d9a7
collins <collinsleonkimani@outlook.com>
2023-03-21 23:17:28 +0300

    done

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  if(400>=value){
+    return "this one is on me"
+  }else if(2001>=value ){
+    return "I will gladly take your thirty bucks"
+  }else if(2501>=value){
+    return"No can do"
+  }
+}
+function ternaryCheckCity(city){
+
+  return city === 'NYC' ? "Ok, sounds good." : "No go."
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch(tip){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous' :
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
